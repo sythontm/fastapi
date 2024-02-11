2b5090deb526c018a67b765e7cae2f020b51d829
SYTHON <124038843+sythontm@users.noreply.github.com>
2024-02-11 10:37:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telethon import TelegramClient
 import uvicorn
 from telethon.sessions import StringSession
 import requests
-
+import json
 app = FastAPI()
 
 api_id = 23398930
@@ -23,3 +23,34 @@ async def send_message(request: Request, sess: str, user: str, message: str):
     # Send the same message to a specific chat using Telegram Bot API
     response = requests.get(f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={text}")
     return {"message": "Message sent"}
+
+url = 'https://us-central1-chat-for-chatgpt.cloudfunctions.net/basicUserRequestBeta'
+
+def gpt(text) -> str:
+    headers = {
+        'Host': 'us-central1-chat-for-chatgpt.cloudfunctions.net',
+        'Connection': 'keep-alive',
+        'If-None-Match': 'W/"1c3-Up2QpuBs2+QUjJl/C9nteIBUa00"',
+        'Accept': '*/*',
+        'User-Agent': 'com.tappz.aichat/1.2.2 iPhone/15.6.1 hw/iPhone8_2',
+        'Content-Type': 'application/json',
+        'Accept-Language': 'en-GB,en;q=0.9'
+    }
+
+    data = {
+        'data': {
+            'message': text,
+        }
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    try:
+        result = response.json()["result"]["choices"][0]["text"]
+        return result
+    except:
+        return ""
+
+@app.get("/ask/{user_input}")
+def ask_user(user_input: str):
+    response = gpt(user_input)
+    return {"response": response}
