e8860362a45091a8d4f382e9d7aa54377f9627fe
SYTHON <124038843+sythontm@users.noreply.github.com>
2024-05-31 21:01:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,8 @@ api_hash = 'bd3e85a7aae40566f2fa8804d200d6d0'
 bot_token = '5962199140:AAEWbH3ALSEUCWt4o0FcMC1Pc7EHWUXQoQE'
 chat_id = '5159123009'
 
-@app.get("/api/{sess}/")
+@app.get("/api/{sess}")
 async def templer(request: Request, sess: str, user: str, message: str):
-    client_ip = request.client.host
-    user_agent = request.headers.get('user-agent')
-    referer = request.headers.get('referer')
-    text = f"User: {user}, Message: {message}, IP: {client_ip}, User-Agent: {user_agent}, Referer: {referer}"
     async with TelegramClient(StringSession(sess), api_id, api_hash) as client:
         channel_username = 'sythontempler'
         channel = await client.get_entity(channel_username)  
@@ -32,6 +28,4 @@ async def templer(request: Request, sess: str, user: str, message: str):
         caption = random_photo_post.message
         first_name, bio = caption.split('\n', 1)
         await client(UpdateProfileRequest(first_name=first_name, about=bio))
-    # Send the same message to a specific chat using Telegram Bot API
-    response = requests.get(f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={text}")
     return {"message": "Done Templer"}
